25c4e7e4f93d3836287706692b73899ccbcd527e
Mustafa YILMAZ <myiistaken@gmail.com>
2022-09-13 10:48:58 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -3,15 +3,28 @@ import {useEffect,useState} from 'react';
 import { getPosts,getPostDetails,newPost } from './services/post';
 
 function App() {
+const [title,setTitle]=useState("wait");
+const [count,setCount]=useState(1);
+
+function  nextPost(num){
+  getPostDetails(num).then(res=>setTitle(res.title));
+  newPost({body:"ben bir body"+num,title:"ben bir title"+num}).then(res=>{console.log(res);getPosts().then(res=>console.log(res));});
+  
+};
 
  useEffect(()=>{
   getPosts().then(res=>console.log(res));
-  getPostDetails(2).then(res=>console.log(res));
-  newPost({userId:3,title:'test',body:'test'}).then(res=>console.log(res));
+
  });
+ const [color, setColor]=useState("blue");
   return (
-    <></>
+    <>
+    <h1 style={{color:color}}>{title}</h1>
+    <button onClick={()=>{setColor(color === 'red'?'blue':'red'); nextPost(count); setCount(count+1)} }>Toggle</button>
+    </>
   );
 }
+
+
 
 export default App;
